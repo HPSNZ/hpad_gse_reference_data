c8affbe00687dfeac019ff668cbf30ff1b02e443
Jacquie Tran <jacquie.tran@gmail.com>
2020-08-19 14:41:18 +1200

    Fixed up tidying of reference data.

--- R/convert_sav_to_csv.R
+++ R/convert_sav_to_csv.R
@@ -4,6 +4,7 @@ library(here)
 library(foreign)
 library(readr)
 library(dplyr)
+library(stringr)
 
 # With thanks to this GitHub gist:
 # https://gist.github.com/igorbrigadir/0d03171e3e731b615f55
@@ -34,8 +35,40 @@ reference_data_tidy <- reference_data_tidy %>%
   filter(obs_id != "sample")
 
 # Remove empty rows used as visual separators for human readability
-reference_data_tidy <- reference_data_tidy %>%
-  filter(self1 != "")
+reference_data_tidy %>%
+  filter(self1 != "") -> reference_data_tidy
+
+# Fix up right-shifted data ----------------------------------------------------
+
+# Subset to right-shifted records and tidy
+reference_data_tidy %>%
+  filter(!sex %in% c(NA, "male", "female")) %>%
+  # Replace values that have shifted over to the incorrect variable
+  mutate(
+    nation = sample,
+    sample = age,
+    age = sex,
+    sex = self1,
+    self1 = self2,
+    self2 = self3,
+    self3 = self4,
+    self4 = self5,
+    self5 = self6,
+    self6 = self7,
+    self7 = self8,
+    self8 = self9,
+    self9 = self10,
+    self10 = agegr_us,
+    agegroup = sumscore,
+    sumscore = NA_real_) -> reference_data_shifted
+
+# Subset to non-shifted records
+reference_data_tidy %>%
+  filter(sex %in% c(NA, "male", "female")) -> reference_data_not_shifted
+
+# Merge subsets back together
+reference_data_tidy <- rbind(
+  reference_data_not_shifted, reference_data_shifted)
 
 # Export the tidy CSV data -----------------------------------------------------
 
